e5cfc6d96a775a981171a66a225eaf2a5f7098a8
MikeNoriegaC <mikenoriegachaves@gmail.com>
2022-10-26 12:32:02 +0200

    fgf

--- src/PracticaI_Mike_NoriegaChaves/Ejercicio1.java
+++ src/PracticaI_Mike_NoriegaChaves/Ejercicio1.java
@@ -4,49 +4,36 @@ import java.util.Scanner;
 
 public class Ejercicio1 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cliente = 0;
+        System.out.println("Introduzca 1 para la primera pregunta");
+        cliente = sc.nextInt();
+
+
         boolean hipoteca = true;
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("¿Quiere solicitar una hipoteca?Introduzca true(si) o false(no)");
-        hipoteca = sc.nextBoolean();
+        switch (cliente) {
+
+            case 1:
 
 
-        if (hipoteca) {
-
-            System.out.println("Perfecto, pero para que se la concedamos necesita cumplir con unos requisitos");
-        }
-
-            else
-            {
-
-                System.out.println("Salgase de la pagina entonces:)");
-            }
+                Scanner sc1 = new Scanner(System.in);
+                System.out.println("¿Quiere solicitar una hipoteca?Introduzca true(si) o false(no)");
+                hipoteca = sc1.nextBoolean();
 
 
+                if (hipoteca) {
 
-            boolean trabajo = true;
-        Scanner sc1 = new Scanner(System.in);
-        System.out.println("¿Usted trabaja?Introduzca true(si) o false(no)");
-        trabajo = sc1.nextBoolean();
+                    System.out.println("Perfecto, pero para que se la concedamos necesita cumplir con unos requisitos");
+                } else {
 
-        if (trabajo) {
-
-            System.out.println("Perfecto ya cumple con uno de los 3 requisitos restantes");
-        }
-
-        else
-        {
-
-            System.out.println("Lo sentimos mucho pero no podemos concederle la hipoteca");
-        }
-
-
-
-
-
+                    System.out.println("Salgase de la pagina entonces:)");
+                    break;
+                }
 
 
         }
     }
+}
 
 
--- src/PracticaI_Mike_NoriegaChaves/Ejercicio2.java
+++ src/PracticaI_Mike_NoriegaChaves/Ejercicio2.java
@@ -1,5 +1,63 @@
 package PracticaI_Mike_NoriegaChaves;
 
+import java.util.Scanner;
+
 public class Ejercicio2 {
+    public static void main(String[] args) {
+        // pedimos al usuario que elija la operacion que quiere que se realice
+        Scanner sc2 = new Scanner(System.in);
+        int calculo=0;
+
+        // Utilizamos un do while para que cada vez que termine de hacer un calculo te vuelva al menu y puedas volver a elegir otra opcion
+        do {
+
+        System.out.println("Escriba 1 si quiere el perimetro, 2 si el area y 3 si el volumen");
+        calculo = sc2.nextInt();
+
+        // declaramos la constante PI
+        final double PI = 3.146;
+
+        // declaramos las variables
+        double area;
+        double radio;
+        double perimetro;
+        double altura;
+        double volumen;
+
+        //aqui estan todos los casos con sus respectivas formulas y resultados
+        switch (calculo) {
+            case 1:
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Escribe el valor del radio: ");
+                radio = sc.nextDouble();
+                perimetro = 2 * PI * radio;
+                System.out.println("El perimetro del círculo es: " + perimetro );
+                break;
+            case 2:
+                Scanner sc1 = new Scanner(System.in);
+                System.out.println("Escribe el valor del radio: ");
+                radio = sc1.nextDouble();
+                area = PI * radio * radio;
+                System.out.println("El área del círculo es: " + area );
+                break;
+            case 3:
+                Scanner sc4 = new Scanner(System.in);
+                System.out.println("Escribe el valor del radio: ");
+                radio = sc4.nextDouble();
+                System.out.println("Escribe el valor de la altura: ");
+                altura = sc4.nextDouble();
+                volumen = PI * radio * radio * altura;
+                System.out.println("El volumen del circulo es: " + volumen );
+                break;
+            default:
+                System.out.println("ERROR: número incorrecto."); break;
+
+        }
+
+        }while (calculo>0 && calculo<4);
+
+    }
 
 }
+
+
