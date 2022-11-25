112787c46640c2e19c279931ac225f4fd8823517
MikeNoriegaC <mikenoriegachaves@gmail.com>
2022-11-25 09:35:42 +0100

    fgf

---
+++ src/sesion2411/DiasSemanaaaaa.java
@@ -0,0 +1,5 @@
+package sesion2411;
+
+public enum DiasSemanaaaaa {
+    LUNES,MARTES,MIERCOLES,JUEVES,VIERNES,SABADO,DOMINGO
+}
---
+++ src/sesion2411/simulacionsemaforo.java
@@ -0,0 +1,13 @@
+package sesion2411;
+enum semaforo{
+    ROJO,AMBAR,VERDE
+}
+
+public class simulacionsemaforo {
+    public static void main(String[] args) {
+
+        semaforo estado_semaforo=semaforo.ROJO;
+        System.out.println(estado_semaforo);
+
+    }
+}
